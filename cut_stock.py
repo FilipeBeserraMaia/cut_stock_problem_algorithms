5537ad89444b43885a32f8c0b4cfa0b7a4a5a049
Cangerana <mrjoaovictorc@gmail.com>
2022-01-05 22:27:38 -0300

    Added GRASP

--- cut_stock.py
+++ cut_stock.py
@@ -1,12 +1,62 @@
 from math import ceil
-from random import uniform
+from random import randint, uniform
+import numpy as np
+import random
+
+
+def calc_solution(solution, cut_value, base_bar):
+    if solution:
+        current_bar = sum(solution[-1])
+    else:
+        current_bar = 0
+        solution = [[]]
+        
+    if  current_bar + cut_value <= base_bar:
+        solution[-1]+= [cut_value]
+    else:
+        solution.append([cut_value])
+
+    return solution
+
+
+def grasp(cut_list=[], base_bar=0):
+    ALPHA = 0.4
+    base_bar = 50
+    cut_list = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+    cut_list = np.array(cut_list)
+
+    indexs = list(range(len(cut_list)))    
+    solution = []
+
+    while indexs:
+        rlc = []
+        maxim = max(cut_list[indexs], key=lambda x: x )
+        
+        minim = min(cut_list[indexs], key=lambda x: x )
+   
+        b = minim + ALPHA * (maxim - minim)
+
+        for i in indexs:
+            if (cut_list[i]) <= b:
+                rlc.append(i)
+
+        if rlc:
+            selected_c = random.uniform(0, len(rlc) - 1)
+            selected_c = rlc[int(selected_c)]
+        else:
+            break
+
+        solution = calc_solution(solution, cut_list[selected_c], base_bar)
+        indexs.pop(indexs.index(selected_c))
+
+    return solution
 
 
 def cut_stock(list_bars, size_base):
     amt_used = 1
     cut_list = {}
-    current_bar = size_base 
-    
+    current_bar = size_base
+
     list_bars.sort(reverse=True)
 
     while list_bars:
@@ -24,8 +74,8 @@ def cut_stock(list_bars, size_base):
     return cut_list, (current_bar, amt_used)
 
 
-#salva a ordem e a barra que o index foi cortado
-def save_cut(cut_list,bar_id,new_item):
+# salva a ordem e a barra que o index foi cortado
+def save_cut(cut_list, bar_id, new_item):
     if not bar_id in cut_list.keys():
         cut_list[str(bar_id)] = []
 
@@ -33,26 +83,22 @@ def save_cut(cut_list,bar_id,new_item):
 
     return cut_list
 
-    
-def search_best_item(items=[], rest=0):
-    """
-    Search the largest 
-    """
-
-    best_index = list(filter(lambda x: x[1] <= rest, enumerate(items)))
+def search_best_item(cut_list=[], rest=0):
+    best_index = list(filter(lambda x: x[1] <= rest, enumerate(cut_list)))
 
     return best_index[0][0] if best_index else None
 
 
 def main():
-    list_bars = [9,2]
+    list_bars = [9, 2]
     size_base = 10
 
     print(sum(list_bars)/size_base)
-   
-    s0 = cut_stock(list_bars,size_base)
+
+    s0 = cut_stock(list_bars, size_base)
 
     print(s0)
 
+
 if __name__ == '__main__':
-    main()
+   print( grasp())
---
+++ grasp.py
@@ -0,0 +1,79 @@
+from random import randint, uniform
+import numpy as np
+import random
+
+def get_value_set(set, vw):
+    total_value = 0
+    total_weigth = 0
+
+    for i in range(len(set)):
+        if set[i] == 1:
+            v = vw[i][0]
+            w = vw[i][1]
+            total_value += v
+            total_weigth += w
+    return total_value, total_weigth
+
+def new_matrix(l, c, w, vw):
+    new_m = np.full(c, 0)
+    removed = []
+    weigth = 0
+
+    new_m = grasp(vw, removed, new_m, w)
+
+    return new_m
+
+def get_value(kps, items):
+    valuer, weigth = 0
+    for i, v in enumerate(items):
+        if kps[i]:
+            valuer += v[0]
+            weigth += v[1]
+
+def grasp(items, removed, m, w):
+    MAX = 0.6
+    MIN = 0.4
+    ALPHA = MAX
+   
+    weigth = 0
+    indexs = list(range(len(items)))
+    rlc = []
+
+    while weigth < w and indexs:
+        rlc = []
+        maxim = max(items[indexs], key=lambda x: (x[0] / x[1]))
+        maxim = maxim[0] / maxim[1]
+        minim = min(items[indexs], key=lambda x: (x[0] / x[1]))
+        minim = minim[0] / minim[1]
+
+        b = minim + ALPHA * (maxim - minim)
+
+        for i in indexs:
+            if (items[i][0] / items[i][1]) >= b:
+                rlc.append(i)
+
+        if rlc:
+            selected_c = random.uniform(0, len(rlc) - 1)
+            selected_c = rlc[int(selected_c)]
+        else:
+            break
+
+        m[selected_c] = 1
+        _, weigth = get_value_set(m, items)
+        
+        indexs.pop(indexs.index(selected_c))
+
+    return m
+
+def main():
+    v_w = [[randint(600, 800), randint(30, 60)] for _ in range(10)]
+    max_weight = 170
+
+    v_w = sorted(v_w, key=lambda i: i[0] / i[1], reverse=True)
+
+    v_w = np.array(v_w)
+
+    print(new_matrix(len(v_w), len(v_w), max_weight, v_w))
+
+if __name__ == '__main__':
+    main()
