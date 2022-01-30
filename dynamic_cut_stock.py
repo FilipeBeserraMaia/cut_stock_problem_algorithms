7f666d7df5f7daff794fcc672e008b865ee856c7
joao.rocha <joao.rocha@siedos.com.br>
2022-01-30 18:22:04 -0300

    add Dynamid and some instances

--- dynamic_cut_stock.py
+++ dynamic_cut_stock.py
@@ -1,54 +1,78 @@
+from unittest import result
 import numpy as np
 import math
 
 def main():
     bottom_up(10, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-def dynamic(cut_list=[], len_bar=10):
-    matrix = np.zeros((len(cut_list), len(len_bar)))
-    cut_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-    for cut in cut_list:
-        for i in range(matrix.shape[0]):
-            for j in range(matrix.shape[1]):
-                if cut <= j:
-                    pass  # fazer o paranaue
-
-    return matrix[-1][-1]
-
-def bottom_up(n, p):
-    size_bar = 7
-    items = [1,2,3,4,5,6]
-
-    max_amt_bar = int(math.ceil(sum(items) / size_bar))
-
-    shape = (len(items) + 1, size_bar + 1, max_amt_bar+1)
-    matrix = np.zeros(shape=shape)
-    next_index = 1
+def dynamic(items, size_bar):
+    shape = (len(items) + 1, size_bar + 1)
+    matrix = np.full(fill_value={'value': 0, 'cuts': []}, shape=shape)
 
     for curr_cut in range(1, matrix.shape[0]):
         for curr_lim_bar in range(1, matrix.shape[1]):
-            best_local_bar = (0, 0)
+            local_bar = -1
+            
+            if items[curr_cut-1] <= curr_lim_bar :
+                local_bar = items[curr_cut-1] + matrix[curr_cut - 1, curr_lim_bar - items[curr_cut-1]]['value']
+                path = [curr_cut-1] + matrix[curr_cut - 1, curr_lim_bar - items[curr_cut-1]]['cuts']
+            
+            previous = matrix[curr_cut - 1, curr_lim_bar]['value']
+            aux = max(local_bar, previous)
 
-            for curr_bar in range(1, matrix.shape[2]):
+            if local_bar < previous:
+                path = matrix[curr_cut - 1, curr_lim_bar]['cuts']
+
+            matrix[curr_cut, curr_lim_bar] = {'value': aux, 'cuts': path}
+
+    return matrix[-1][-1]
 
 
-                local_bar = 0
 
-                if items[curr_cut-1] <= curr_lim_bar:
-                    local_bar = items[curr_cut-1] + matrix[curr_cut - 1, curr_lim_bar - curr_cut, curr_bar]
+def bottom_up(items, size_bar):
+    # size_bar = 10
+    # items = [1,2,3,4,5,6]
 
-                previous = matrix[curr_cut - 1, curr_lim_bar, curr_bar]
-                aux = max(local_bar, previous)
+    max_amt_bar = int(math.ceil(sum(items) / size_bar))
+    result_list = []
 
-                if aux > best_local_bar[1]:
-                    best_local_bar = (curr_bar, aux)
-            matrix[curr_cut, curr_lim_bar, best_local_bar[0]] = best_local_bar[1]
+    while len(items) > 0:
+        result = dynamic(items, size_bar)
+        result_list.append([items[i] for i in result['cuts']])
+        items = np.delete(items, result['cuts'])
+        
+        print(result)
+    print(result_list)
+    print(len(result_list))
+ 
+   
+    # for i in range(matrix.shape[2]):
+    #     print(matrix[-1][-1][i])
+    # return matrix[-1][-1][-1]
 
-    for i in range(matrix.shape[2]):
-        print(matrix[-1][-1][i])
-    return matrix[-1][-1][-1]
+def read_instances(path):
+    with open(path ,'r') as f:
+        data = f.read()
+        data = data.split("\n")
+
+    result = int(data.pop(0))
+    size_bar = int(data.pop(0))
+    
+    cuts = []
+    for lines in data:
+        line = lines.split('\t')
+        if len(line) > 1:
+            cuts += [int(line[0])] * int(line[1])
+
+    return cuts, size_bar, result
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    
+    cuts, size_bar, result = read_instances('instancias/Solutionsfaceis/Schwerin1_BPP1.txt')
+    print(len(cuts))
+    # print(sum(cuts))
+    cuts.sort()
+    bottom_up(cuts, size_bar)
+
